8ca2b6d7103d67c723a60af24b99f71e48da4c00
Wouter van Wijk <woutervanwijk@gmail.com>
2022-11-30 11:40:11 +0100

    add mastodon logo

--- src/components/icons/icon.js
+++ src/components/icons/icon.js
@@ -15,6 +15,7 @@ import {
   IconPlayStore,
   IconStar,
   IconTwitter,
+  IconMastodon,
 } from '@components/icons';
 
 const Icon = ({ name }) => {
@@ -47,6 +48,8 @@ const Icon = ({ name }) => {
       return <IconStar />;
     case 'Twitter':
       return <IconTwitter />;
+    case 'Mastodon':
+      return <IconMastodon />;
     default:
       return <IconExternal />;
   }
